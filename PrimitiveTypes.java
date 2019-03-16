96dcc0f51ebcf537c3595d93ffb3a21e36abced5
MissSaumya <43300802+MissSaumya@users.noreply.github.com>
2019-03-16 11:01:26 +0530

    Add files via upload

---
+++ PrimitiveTypes.java
@@ -0,0 +1,22 @@
+class  PrimitiveTypes
+{
+    static byte bytte;
+    static short shortt;
+    static int intt;
+    static long longg;
+    static float floatt;
+    static double doublee;
+    static char charr;
+    static boolean b;
+    public static void main(String args[]) 
+    {
+        System.out.println("Byte :"+bytte);
+        System.out.println("Short :"+shortt);
+        System.out.println("Int :"+intt);
+        System.out.println("Long :"+longg);
+        System.out.println("Float :"+floatt);
+        System.out.println("Double :"+doublee);
+        System.out.println("Char :"+charr);
+        System.out.println("Boolean :"+b);
+    }
+}
